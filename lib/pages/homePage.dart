5f4e5e7696b4fecef9f1837d41ce0b24c49473fb
Leandro Gabatel <legabatel@hotmail.com>
2020-06-02 21:24:08 -0300

    Medição
    aaaaaaa

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -1,7 +1,9 @@
+import 'package:dart_amqp/dart_amqp.dart';
 import 'package:barcode_scan/barcode_scan.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:app_vendas_treino/shared/sharedConstants.dart' as constants;
+import 'package:http/http.dart' as http;
 
 import 'dart:convert';
 
@@ -62,16 +64,87 @@ class ListaScreen extends State<HomePage>{
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.swap_horizontal_circle, color: Colors.white),
-        onPressed: () {
-          setState(() async {
+        onPressed: () async {
+          await startMeasure(context, "");
+          /*setState(() async {
             var result = await BarcodeScanner.scan();
-
-            print(result);
-          });
+            await startMeasure(context, result);
+          });*/
         },
         backgroundColor: Color(0xff5d0dff),
       ),
       body: Center(child: Text('Bem vindo ao FootUnity'),),
     );
   }
+}
+
+startMeasure(context, macAdrress) async {
+  var prefs = await SharedPreferences.getInstance();
+  var authData = prefs.getString(constants.AuthKey);
+  var auth = jsonDecode(authData);
+
+  var connectionSettings = new ConnectionSettings(
+    host: constants.RabbitMQHost,
+    port: constants.RabbitMQPort,
+    authProvider: constants.RabbitMQAuth,
+    virtualHost: '/'
+  );
+
+  Client client = Client(settings: connectionSettings);
+
+  var jsonPost = 
+  { 
+    "MacAddress": macAdrress,
+    "UserId": auth['id'],
+  };
+  BuildContext dialogContext;
+
+  showDialog(
+    context: context,
+    barrierDismissible: false,
+    builder: (BuildContext context) {
+      dialogContext = context;
+        return AlertDialog(
+          content: new Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Center(
+                child: new SizedBox(
+                  height: 50.0,
+                  width: 50.0,
+                  child: new CircularProgressIndicator(
+                    value: null,
+                    strokeWidth: 7.0,
+                  ),
+                ),
+              ),
+              new Container(
+                margin: const EdgeInsets.only(top: 25.0),
+                child: new Center(
+                  child: new Text(
+                    "Aguardando medição...",
+                    style: new TextStyle(
+                      color: Color(0xff5d0dff)
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+    }
+  );
+
+  http.post(constants.FootAPILink + 'message/startmeasure', body: jsonPost);
+
+  client
+    .channel()
+    .then((Channel channel) => channel.queue("ReturnMeasure"))
+    .then((Queue queue) => queue.consume())
+    .then((Consumer consumer) => consumer.listen((AmqpMessage message) {
+      var json = message.payloadAsJson;
+      Navigator.pop(dialogContext);
+      client.close();
+    }));
 }
--- lib/shared/sharedConstants.dart
+++ lib/shared/sharedConstants.dart
@@ -1,4 +1,9 @@
+import 'package:dart_amqp/dart_amqp.dart';
+
 const String LoginKey = 'loginDataKey';
 const String AuthKey = 'authDataKey';
 const String AuthAPILink = 'http://footunity.ddns.net/api/auth';
 const String FootAPILink = 'http://footunity.ddns.net/api/';
+const String RabbitMQHost = 'footunity.ddns.net';
+const PlainAuthenticator RabbitMQAuth = PlainAuthenticator("admin", "senhabemsecreta");
+const int RabbitMQPort = 5672;
